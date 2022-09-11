9b53bfbf50882dde33687cde76377d31fed7270d
Naitik <naitikjkapadia@gmail.com>
2022-09-11 23:18:29 +0530

    Done

--- pages/pendingrequest.js
+++ pages/pendingrequest.js
@@ -2,10 +2,12 @@ import Head from "next/head";
 import {useSession} from "next-auth/react";
 import { NextResponse } from "next/server.js";
 import LoginTempComponent from "../components/LoginTempComponent.jsx";
-import Dashboard from "../components/TeamDashboard/Dashboard.jsx";
+// import Dashboard from "../components/Dashboard.jsx";
 import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router.js";
+import SearchTeams from "../components/SearchTeams.jsx";
+import PendingRequests from "../components/PendingRequests.jsx"
 
 // this is Landing page, here people will learn about the event and signup/login
 
@@ -18,8 +20,8 @@ export default function Home() {
   useEffect(()=>{
     if (!router.isReady) return;
 
-    if (session){
-      router.push('/dashboard')
+    if (session===null){
+      router.push('/')
     }
   }, [router.isReady, session,router])
 
@@ -32,9 +34,9 @@ export default function Home() {
       </Head>
 
       <h1 className={styles.title}>Futurepreneurs</h1>
-
-      <LoginTempComponent />
-      {/* <Dashboard/> */}
+      <PendingRequests/>
+      {/* <LoginTempComponent />
+      <Dashboard/> */}
     </div>
   );
 }
