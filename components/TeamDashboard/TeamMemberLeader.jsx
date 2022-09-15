eef887b4a6a82baf1662f04b0b9933c06f25af38
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2022-09-15 18:39:02 +0530

    fixed remove member function

--- components/TeamDashboard/TeamMemberLeader.jsx
+++ components/TeamDashboard/TeamMemberLeader.jsx
@@ -1,54 +1,67 @@
 import styles from "../../styles/Dashboard.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useSession } from "next-auth/react";
-const TeamMemberLeader = ({ teamName, mobileNumber, email, teamId, userId, handleMemberRemove, teamRole }) => {
-
+const TeamMemberLeader = ({
+  teamName,
+  mobileNumber,
+  email,
+  teamId,
+  userId,
+  handleMemberRemove,
+  teamRole,
+}) => {
   const { data: session } = useSession();
   const handleRemove = () => {
     fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/team/remove/${teamId}`, {
       method: "PATCH",
-      body:
-        JSON.stringify({
-          "userId": `${userId}`,
-        }),
+      body: JSON.stringify({
+        userId: `${userId}`,
+      }),
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${session.accessTokenBackend}`,
         "Access-Control-Allow-Origin": "*",
       },
     })
-      .then(data => data.json())
-      .then(data => {
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        toast.success(`${data.message}`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
         handleMemberRemove();
-      })
-  }
+      });
+  };
 
   return (
     <div
       className={`${styles.col} ${styles.lg4} ${styles.md3} ${styles.xs2} ${styles.flex_stretch}`}
     >
+      <ToastContainer />
       <div className={styles.member_container}>
-        <div
-          className={`${styles.centre_align} ${styles.bottom_margin}`}
-        >
+        <div className={`${styles.centre_align} ${styles.bottom_margin}`}>
           <h4 className={styles.member_name}>{teamName}</h4>
-          {teamRole == 0 ?
+          {teamRole == 0 ? (
             <p className={styles.role_tag}>Leader</p>
-            :
+          ) : (
             <p className={styles.role_tag}>Teammate</p>
-          }
+          )}
         </div>
         <div className={`${styles.centre_align} ${styles.bottom_margin}`}>
           <p className={styles.phone_number}>{mobileNumber}</p>
           <p className={styles.paragraph}>{email}</p>
 
-          {teamRole == 0 ?
+          {teamRole == 0 ? (
             <></>
-            :
-            <button className={`${styles.remove_team_btn} ${styles.w_button} `} onClick={handleRemove}>
+          ) : (
+            <button
+              className={`${styles.remove_team_btn} ${styles.w_button} `}
+              onClick={handleRemove}
+            >
               Remove
             </button>
-          }
-
+          )}
         </div>
       </div>
     </div>
