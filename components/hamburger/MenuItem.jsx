e67b3ab255d1d91d22a4202687a484a43249511a
Pratyush <connect2pratyush@gmail.com>
2022-09-18 23:36:22 +0530

    Main Timer

---
+++ components/hamburger/MenuItem.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import styles from "../../styles/Hamburger.module.css"
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
+
+export const MenuItem = ({ i }) => {
+  const style = { border: `2px solid ${colors[i]}` };
+  return (
+    <motion.li className={styles.li}
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <div className={styles.icon_placeholder} style={style} />
+      <div className={styles.text_placeholder} style={style} />
+    </motion.li>
+  );
+};
---
+++ components/hamburger/Navigation.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import { MenuItem } from "./MenuItem";
+import styles from "../../styles/Hamburger.module.css"
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
+export const Navigation = () => (
+  <motion.ul className={styles.ul} variants={variants}>
+    {itemIds.map(i => (
+      <MenuItem i={i} key={i} />
+    ))}
+  </motion.ul>
+);
+
+const itemIds = [0, 1, 2, 3, 4];
