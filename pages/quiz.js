105a1c9904ef712935d44fddf8580498923eb272
Pratyush <connect2pratyush@gmail.com>
2022-09-25 21:51:14 +0530

    nextQuestion

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Questions from "../components/Questions";
-import QuestionsMatch from "../components/QuestionsMatch";
+
 export default function Quiz() {
-return(
-    <QuestionsMatch/>
-);
+  return(
+      <Questions/>
+  );
 }
