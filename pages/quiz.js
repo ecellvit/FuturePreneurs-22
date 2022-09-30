216a8f1a6f0ff897777a7b553f8eec2c113ac931
Chiranjeev Vishnoi <somu209e@gmail.com>
2022-10-01 02:37:38 +0530

    fixed minor bugs

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Questions from "../components/quizQuestions/Questions";
+import { useSession } from "next-auth/react";
 
 export default function Quiz() {
-  return(
-      <Questions/>
-  );
+  return <Questions />;
 }
