a03d981cc8af83e51eb3917609a080975ef3405d
Anjy Gupta <anjyofficial@gmail.com>
2022-10-16 11:47:42 +0530

    fixed instructions page

--- pages/Instructions.js
+++ pages/Instructions.js
@@ -1,11 +1,40 @@
-
-import CardComponent from "../components/CardComponent";
-import styles from "../styles/Dashboard.module.css";
+import CardComponent from '../components/CardComponent'
+import styles from '../styles/Dashboard.module.css'
 export default function Instructions() {
+  const [round, setRound] = useState()
+
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/team/quiz/${TEAM_ID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.accessTokenBackend}`,
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(respBody),
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {})
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <div className={styles.cardbody}>
-          <CardComponent/>
-      </div>
-  );
+    {
+     (round === "round1")&&<CardComponent heading={one} />
+    }
+    {
+     (round === "round2")&&<CardComponent heading={two} />
+    }
+    {
+     (round === "round2")&&<CardComponent heading={three}/>
+    }
+
+    </div>
+  )
 }
