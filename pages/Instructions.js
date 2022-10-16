7f6d557b0ffbb785d3d6db464a7b956f42e9e469
Anjy Gupta <anjyofficial@gmail.com>
2022-10-16 11:44:02 +0530

    Added instructions component and  page

---
+++ pages/Instructions.js
@@ -0,0 +1,11 @@
+
+import CardComponent from "../components/CardComponent";
+import styles from "../styles/Dashboard.module.css";
+export default function Instructions() {
+
+  return (
+    <div className={styles.cardbody}>
+          <CardComponent/>
+      </div>
+  );
+}
