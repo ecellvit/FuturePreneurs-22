4a86d5f246292aaba3ba8fca97f258c60ad14d55
Naitik <naitikjkapadia@gmail.com>
2022-10-17 13:07:58 +0530

    Sponsors Added

--- components/landing_main/Section6.jsx
+++ components/landing_main/Section6.jsx
@@ -4,8 +4,15 @@ import Image from "next/image";
 import MG from "./MG.jpeg";
 import ELM from "./ELM.jpeg";
 import WF from "./WF.png";
+import WSS from "./WSS.jpg";
+import TIIM from "./TIIM.JPG";
+import BN from "./BN.png";
+import ES from "./ES.png";
+import IDP from "./IDP.jpg";
+import OFW from "./OFW.jpeg";
+import VESIT from "./VESIT.jpg";
+
 import { Rotating } from "../animationComponents/Rotating";
-import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Section6 = () => {
   return (
@@ -14,15 +21,130 @@ const Section6 = () => {
       <img src="dffj.svg" width="288.5" className={styles.spon_grad_r} />
       <img src="gs.svg" width="303.5" className={styles.spon_grad_l} />
       <div className={styles.Sponsors}>
-        <div className={styles.Cards}>
-          <Image src={MG} />
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a href="https://www.instagram.com/magizham_gardens/" target="_blank">
+            <Image src={MG} />
+          </a>
         </div>
 
-        <div className={styles.Cards}>
-          <Image src={ELM} />
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a href="https://www.elearnmarkets.com/" target="_blank">
+            <Image src={ELM} />
+          </a>
         </div>
-        <div className={styles.Cards}>
-          <Image src={WF} />
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a href="https://www.wolfram.com/" target="_blank">
+            <Image src={WF} />
+          </a>
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a href="https://wharfstreetstudios.com/" target="_blank">
+            <Image src={WSS} />
+          </a>
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="https://www.barbequenation.com/"
+            target="_blank"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Image src={BN} />
+          </a>
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="https://www.eatsure.com/"
+            target="_blank"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Image src={ES} />
+          </a>
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Image src={IDP} />
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Image src={OFW} />
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href="https://online.2iim.com/cat-exam-preparation-online-cat-coaching.shtml"
+            target="_blank"
+          >
+            <Image src={TIIM} />
+          </a>
+        </div>
+        <div
+          className={styles.Cards}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {" "}
+          <a href="https://instagram.com/ve_sit_restaurant" target="_blank">
+            <Image src={VESIT} />
+          </a>
         </div>
       </div>
     </div>
